85a91e1cb575f2d0ddc5af38cc5dd585fdb3b790
Exacttime <112515202+Exacttime@users.noreply.github.com>
2023-06-04 20:09:26 -0300

    Update card-item.component.ts

--- src/app/Components/card-item/card-item.component.ts
+++ src/app/Components/card-item/card-item.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
 import { ApiService } from 'src/app/recipe-api.service';
-import { HttpClient } from '@angular/common/http';
 import { CardClass } from '../CardClass';
 @Component({
   selector: 'app-card-item',
