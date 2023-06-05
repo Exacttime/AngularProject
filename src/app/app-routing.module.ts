1fa1ee87fc6123e63deededebf1c256f2857ba0c
Exacttime <112515202+Exacttime@users.noreply.github.com>
2023-06-04 21:13:46 -0300

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '',redirectTo: 'folder/Inbox', pathMatch: 'full' },
   { path: 'folder/:id',loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)},
-  { path: 'pages/recipepage', component:RecipepageComponent }
+]
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
