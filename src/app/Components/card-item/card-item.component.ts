bffd50c9fde50fcbcef66666ddae70ed327011af
Exacttime <112515202+Exacttime@users.noreply.github.com>
2023-06-04 21:06:38 -0300

    Git button

--- src/app/Components/card-item/card-item.component.ts
+++ src/app/Components/card-item/card-item.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ApiService } from 'src/app/recipe-api.service';
 import { CardClass } from '../CardClass';
+
 @Component({
   selector: 'app-card-item',
   templateUrl: './card-item.component.html',
   styleUrls: ['./card-item.component.scss'],
 })
 
-
 export class CardItemComponent  implements OnInit {
   instancias: CardClass[] = [];
-
   constructor(private apiService: ApiService) {}
-
   ngOnInit() {
     this.apiService.criarInstanciasClasse().subscribe((instancias) => {
       this.instancias = instancias;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { RecipepageComponent } from './pages/recipepage/recipepage.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
+  { path: '',redirectTo: 'folder/Inbox', pathMatch: 'full' },
+  { path: 'folder/:id',loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)},
   { path: 'pages/recipepage', component:RecipepageComponent }
 ];
 
